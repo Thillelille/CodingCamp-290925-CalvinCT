75f22c59061294b07bbf6dea3e320001f4ebc500
Thillelille <calvinct.1998@gmail.com>
2025-10-02 20:56:31 +0700

    all

---
+++ js/script.js
@@ -0,0 +1,26 @@
+console.log("test");
+
+let todo = [];
+
+function renderToDo()
+{
+    document.getElementById
+}
+function addToDo()
+{
+    const todoInput = document.getElementById
+    const todoDate = document.getElementById
+    console.log("ass")
+}
+function deleteddToDo()
+{
+    
+}
+function filterToDo()
+{
+    
+}
+function filterToDo()
+{
+    if (!todo)
+}
